a74546c6a47875498f7c45ca8ed632c9e0b3f932
Ayush Shakya <ayshakya24@gmail.com>
2026-04-14 14:32:53 +0530

    Db and backend synchronization with retry connection setup logic

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,9 @@ dotenv.config();
 
 import app from "./app.js";
 import pool from "./config/postgres.js";
-import redis from "./config/redis.js"
+import redis from "./config/redis.js";
 import { subscribeToPresenceEvents } from "./modules/presence/presence.pubsub.js";
-import { initWebSocket, WS_SERVER_INSTANCE_ID  } from "./websocket/ws.server.js";
+import { initWebSocket, WS_SERVER_INSTANCE_ID } from "./websocket/ws.server.js";
 import { subscribeToDocumentEvents } from "./modules/document/document.pubsub.js";
 import http, { get } from "http";
 import { startDocumentWorkers } from "./modules/document/document.worker.js";
@@ -16,18 +16,37 @@ const server = http.createServer(app);
 const ws = initWebSocket(server);
 startDocumentWorkers();
 
-async function startServer() {
+async function waitForDependencies(retries = 10) {
+  while (retries) {
     try {
-        await pool.query("SELECT 1");
-        await redis.ping();
+      console.log("Checking PostgreSQL...");
+      await pool.query("SELECT 1");
 
-        server.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
+      console.log("Checking Redis...");
+      await redis.ping();
+
+      console.log("All dependencies ready ✅");
+      return;
     } catch (error) {
-        console.error("Failed to start server", error);
-        process.exit(1);
+      console.log("Dependencies not ready, retrying in 3s...");
+      await new Promise((res) => setTimeout(res, 3000));
+      retries--;
     }
+  }
+  throw new Error("Dependencies not ready after retries");
+}
+
+async function startServer() {
+  try {
+    await waitForDependencies();
+
+    server.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server", error);
+    process.exit(1);
+  }
 }
 
 subscribeToPresenceEvents(async (event) => {
@@ -40,9 +59,9 @@ subscribeToPresenceEvents(async (event) => {
 });
 subscribeToDocumentEvents((event) => {
   try {
-    if(event.sourceInstance === WS_SERVER_INSTANCE_ID){
+    if (event.sourceInstance === WS_SERVER_INSTANCE_ID) {
       return;
-    }  
+    }
     ws.broadcastDoc(event.workspaceId, event.docId, event, {
       excludeUserId: event.updatedBy,
     });
